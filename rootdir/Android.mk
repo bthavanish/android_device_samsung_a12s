283ba110a8603c7412fa292e3666d9e2f3482c1c
bthavanish <bthavanish@users.noreply.github.com>
2026-07-08 17:19:23 +0530

    a12s: add kernel build glue, ueventd.rc, enable NFC, fix BoardConfig

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -9,6 +9,14 @@ LOCAL_MODULE_PATH  := $(TARGET_RECOVERY_ROOT_OUT)/first_stage_ramdisk
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE       := ueventd.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/ueventd.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE       := init.exynos850.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
